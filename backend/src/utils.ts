2a292b926c872b1ae5aba649ea617f5fbe481ada
Skyz <skyz@likeswolves.com>
2022-08-11 15:22:53 -0400

    Switch to default emoji

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -573,11 +573,11 @@ export function convertMSToDelayString(ms: number): string {
   return result;
 }
 
-export function successMessage(str, emoji = "<:zep_check:650361014180904971>") {
+export function successMessage(str, emoji = "✅") {
   return emoji ? `${emoji} ${str}` : str;
 }
 
-export function errorMessage(str, emoji = "⚠") {
+export function errorMessage(str, emoji = "❌") {
   return emoji ? `${emoji} ${str}` : str;
 }
 
